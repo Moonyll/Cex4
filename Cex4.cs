9039a16859715c6e2f47fbb4a2e704dc9c73bb5f
Moonyll <s.pouwel@laposte.net>
2018-12-18 14:22:30 +0100

    Cex4

---
+++ Cex4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Identifiant ?");
+            string login = Console.ReadLine();
+            Console.WriteLine("Mot de passe ?");
+            string password = Console.ReadLine();
+            if (login == "stef" && password == "007") // Attention au double ==
+                Console.WriteLine("« Bienvenue à la Manu le Havre. »");
+            else
+                Console.WriteLine("« Mauvais identifiant ou mauvais mot de passe. »");
+        }
+    }
+}
